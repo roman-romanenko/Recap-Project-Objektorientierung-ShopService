eeb6dc3deebfd7909fb563b161f70910be27f795
Roman Romanenko <capital.car7@gmail.com>
2025-12-19 13:45:09 +0100

    fix remove test

--- src/test/java/product/ProductRepoTest.java
+++ src/test/java/product/ProductRepoTest.java
@@ -60,6 +60,6 @@ class ProductRepoTest {
         repo.removeProduct("1");
 
         //THEN
-        assertNull(repo.getProductById("1"));
+        assertTrue(repo.getProductById("1").isEmpty());
     }
 }
